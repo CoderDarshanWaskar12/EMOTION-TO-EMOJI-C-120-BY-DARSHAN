69b2fdacf535e21b1c3fa3184fd21478b18e709d
CoderDarshanWaskar12 <111749254+CoderDarshanWaskar12@users.noreply.github.com>
2022-09-30 19:19:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+prediction_1 = ""
+prediction_2 = ""
+
  Webcam.set({
     width:350,
     height:300,
@@ -26,12 +29,12 @@ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models
 
 function speak(){
     var synth = window.speechSynthesis;
-    speak_data_1 = "The First prediction is " + prediction_1;
-    speak_data_2 = "And the second prediction is" + prediction_2;
+    speak_data_1 = "The first prediction is " + prediction_1;
+    speak_data_2 = "And the second prediction is " + prediction_2;
     var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
     synth.speak(utterThis);
-}
-
+  }
+  
 function check()
 {
     img = document.getElementById('captured_image');
@@ -45,8 +48,8 @@ function gotResult(error, results) {
         console.log(results);
         document.getElementById("results_emotion_name").innerHTML = results[0].label;
         document.getElementById("results_emotion_name2").innerHTML = results[1].label;
-        prediction_1 = reslts[0].label;
-        prediction_2 = reslts[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
         speak();
         if(results[0].label == "happy")
         {
